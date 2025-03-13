ef0c72d8ee1ce73b67c5181b500423e4f3501297
Freddie Feria <freddie.feria2019@gmail.com>
2025-03-13 17:15:39 -0400

    Game and Pieces
    Create a main game that will be where all the game mechanic and structure will be. The pieces is a separate program that will be hosting features of a piece (move, color, position on board)

---
+++ Game.java
@@ -0,0 +1,17 @@
+import Pieces;
+
+public class Game {
+    
+    public class Board {
+        Board [][] board = new Board[8][8]; // Creating the 8x8 board
+        setupBoard(); // Call to set up board
+    }
+
+    private void setupPieces() {
+        
+    }
+
+    public static void main(String args[]){
+    }
+
+}
