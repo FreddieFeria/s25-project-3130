f4795d6fca8c2834a02451b4912ac373005cc578
FreddieFeria <freddie.feria2019@gmail.com>
2025-03-13 17:39:46 -0400

    Changed the setupGame function to a comment so that I can try and set a pull request

--- Game.java
+++ Game.java
@@ -1,10 +1,10 @@
-import Pieces;
+
 
 public class Game {
     
     public class Board {
         Board [][] board = new Board[8][8]; // Creating the 8x8 board
-        setupBoard(); // Call to set up board
+        //setupBoard(); // Call to set up board
     }
 
     private void setupPieces() {
